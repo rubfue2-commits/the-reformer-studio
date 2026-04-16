4337874e3bbc27cf5ba5a172cdcf4edd57bf7217
rubfue2-commits <rubfue2@gmail.com>
2026-04-16 12:10:11 +0200

    Create LanguageContext.tsx

---
+++ src/i18n/LanguageContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState, ReactNode } from "react";
+import { Language, translations, Translations } from "./translations";
+
+interface LanguageContextType {
+  language: Language;
+  setLanguage: (lang: Language) => void;
+  t: Translations;
+}
+
+const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
+
+export const LanguageProvider = ({ children }: { children: ReactNode }) => {
+  const [language, setLanguage] = useState<Language>("en");
+  const t = translations[language];
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage, t }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const ctx = useContext(LanguageContext);
+  if (!ctx) throw new Error("useLanguage must be used within LanguageProvider");
+  return ctx;
+};
